8b5e80e1ebed2d4900fd15ee49d99fd289cfd987
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-13 14:08:19 +0200

    Exercise concluded.

---
+++ solution.js
@@ -0,0 +1,35 @@
+// 1
+//
+// function calcMulti(a, b){
+//   return a * b;
+// };
+// console.log(calcMulti(2, 2));
+
+// 2
+//
+// const calcMulti = function (a, b) {
+//   return a * b;
+// };
+// console.log(calcMulti(2, 2));
+
+// 3
+//
+// const calcMulti = (a, b) => a * b;
+// console.log(calcMulti(2, 2));
+
+// 4
+// function declaration
+// function checkRemainder(a, b) {
+//   return a % b;
+// }
+// console.log(checkRemainder(2, 2));
+
+// function expression
+// const checkRemainder = function (a, b) {
+//   return a % b;
+// };
+// console.log(checkRemainder(2, 2));
+
+// arrow function
+const checkRemainder = (a, b) => a % b;
+console.log(checkRemainder(2, 2));
